71df63b5e41317296e699a09e9e2def4fa6ec894
Piotr Karpala <piotrkarpala@microsoft.com>
2023-11-10 11:23:25 -0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

---
+++ screens/AddCity/index.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { SafeAreaView, TextInput, Button, StyleSheet, View, FlatList, Text } from 'react-native';
+
+const AddCityScreen = () => {
+  const [city, setCity] = useState('');
+  const [cities, setCities] = useState([]);
+
+  const addCity = () => {
+    setCities([...cities, {
+      id: Math.random().toString(),
+      value: city
+    }]);
+    setCity('');
+  };
+
+  return <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Enter City" style={styles.input} onChangeText={setCity} value={city} />
+        <Button title="Add City" onPress={addCity} />
+      </View>
+      <FlatList data={cities} keyExtractor={item => item.id} renderItem={itemData => <View style={styles.listItem}>
+            <Text>{itemData.item.value}</Text>
+          </View>} />
+    </SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '80%',
+    marginBottom: 10
+  },
+  input: {
+    width: '70%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
+  }
+});
+export default AddCityScreen;
