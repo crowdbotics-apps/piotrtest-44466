64f04219f48ac2b2eabaa2a29a075a6dd4658a19
crowdbotics-apps <team@crowdbotics.com>
2023-11-10 16:17:45 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,9 @@
+from django.conf import settings
 from django.db import models
+
+
+class City(models.Model):
+    "Generated Model"
+    name = models.TextField()
+    size = models.BigIntegerField()
+    url = models.URLField()
