c24957798e3373dbb4bc3194c20806e296c3d82f
Piotr Karpala <piotrkarpala@microsoft.com>
2023-11-10 11:10:25 -0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

---
+++ screens/ViewData/index.js
@@ -0,0 +1,50 @@
+import { FlatList } from "react-native";
+import { View } from "react-native";
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, ScrollView, Text, StyleSheet } from 'react-native';
+
+const ViewData = () => {
+  const [cities, setCities] = useState([]);
+  useEffect(() => {
+    fetch('https://your-database-url.com/api/cities').then(response => response.json()).then(data => setCities(data)).catch(error => console.error(error));
+  }, []);
+  return <SafeAreaView style={styles.safeArea}>
+      <ScrollView contentContainerStyle={styles.contentContainer}>
+        {cities.map((city, index) => <Text key={index} style={styles.text}>
+            {city.name}
+          </Text>)}
+      <FlatList style={styles.UiZXTRlF} renderItem={({
+        item
+      }) => <View style={styles.lKFvjqAi}></View>} ItemSeparatorComponent={() => <View style={styles.CYZCLJgf} />} data={[1, 2, 3]} keyExtractor={(item, index) => index}></FlatList></ScrollView>
+    </SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1
+  },
+  contentContainer: {
+    backgroundColor: '#f0f0f1',
+    padding: 10
+  },
+  text: {
+    fontSize: 16,
+    color: '#333'
+  },
+  UiZXTRlF: {
+    position: "absolute",
+    width: 275,
+    height: 274,
+    left: 6,
+    top: 311
+  },
+  lKFvjqAi: {
+    position: "absolute",
+    top: 0
+  },
+  CYZCLJgf: {
+    backgroundColor: "#000000",
+    height: 1
+  }
+});
+export default ViewData;
